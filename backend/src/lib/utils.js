2f05bd8ea01a234b6fa5866685f13fdbe9edeae4
Jana <128377430+jana423151@users.noreply.github.com>
2026-07-05 20:48:05 +0300

    Update JWT cookie settings for improved security and compatibility

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,12 +11,12 @@ export const generateToken = (userId, res) => {
         expiresIn: "7d",
     });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-        httpOnly: true, // prevent XSS attacks: cross-site scripting
-        sameSite: "strict", // CSF attacks
-        secure: ENV.NODE_ENV === "development" ? false : true,
-    });
+   res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: ENV.NODE_ENV === "production",
+    sameSite: "none",
+   });
 
     return token;
 };
