3bfac785f4c39959a40246303f793d1e32b6d5d2
Jana <128377430+jana423151@users.noreply.github.com>
2026-07-05 20:38:29 +0300

    debug client url

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
- import express from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import cors from "cors";
@@ -12,6 +12,9 @@ import { app, server } from "./lib/socket.js";
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
+
+console.log("CLIENT_URL =", ENV.CLIENT_URL);
+console.log("NODE_ENV =", ENV.NODE_ENV);
 
 app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
